b7edcf9af04074ce0fc94f7052d6c190f143b720
Labhatorian <16213031+Labhatorian@users.noreply.github.com>
2022-04-20 11:56:03 +0200

    DPToevDialog verbeterd, UUID en icoontjes alleen nog

--- src/main/java/nl/windesheim/ictm2o/peach/DPToevDialog.java
+++ src/main/java/nl/windesheim/ictm2o/peach/DPToevDialog.java
@@ -15,18 +15,23 @@ public class DPToevDialog extends JDialog implements ActionListener {
 
     private JComboBox options = new JComboBox(optionsToChoose);
     private JTextField naam = new JTextField(5);
+    private JTextField prijs = new JTextField(5);
     private JButton toevoegen = new JButton("Toevoegen");
     private JButton cancel = new JButton("Annuleren");
 
     private ComponentRegistry CR;
     public DPToevDialog(JFrame frame, boolean modal, ComponentRegistry CR){
         super(frame, modal);
-        setSize(200,100);
+        setSize(300,110);
         setLayout(new FlowLayout());
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setTitle("Toevoegen component");
+
+        //Hardcoded icoontjes of ophalen?
+
         add(options);
         add(naam);
+        add(prijs);
         add(toevoegen);
         toevoegen.addActionListener(this);
         add(cancel);
@@ -40,8 +45,9 @@ public class DPToevDialog extends JDialog implements ActionListener {
             //Voeg component toe aan lijst en refresh JPanel
             //Eerst componentregistry hier helemaal naartoe halen en dan nieuw component toevoegen
 
-            //RegisteredComponent newComponent = new RegisteredComponent(, naam.getText(), ComponentIcon.GENERIC, 200);
+            //RegisteredComponent newComponent = new RegisteredComponent(, naam.getText(), ComponentIcon.GENERIC, prijs.getText());
             //CR.getRegisteredComponents().add(newComponent);
+            dispose();
         }
         if(e.getSource() == cancel){
             dispose();
