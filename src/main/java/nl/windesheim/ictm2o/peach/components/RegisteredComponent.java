34eee5b34470eb713c63c01a09e9d83c7bace6b1
Tristan <code@thewoosh.org>
2022-04-20 09:56:04 +0200

    AIVD + CIA + FBI

--- src/main/java/nl/windesheim/ictm2o/peach/components/RegisteredComponent.java
+++ src/main/java/nl/windesheim/ictm2o/peach/components/RegisteredComponent.java
@@ -2,16 +2,25 @@ package nl.windesheim.ictm2o.peach.components;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.UUID;
+
 public class RegisteredComponent {
 
+    private final @NotNull UUID id;
     private final @NotNull String name;
     private final @NotNull ComponentIcon icon;
     private final float cost;
 
-    public RegisteredComponent(@NotNull String name, @NotNull ComponentIcon icon, float cost) {
+    public RegisteredComponent(@NotNull UUID id, @NotNull String name, @NotNull ComponentIcon icon, float cost) {
+        this.id = id;
         this.name = name;
         this.icon = icon;
         this.cost = cost;
+    }
+
+    @NotNull
+    public UUID getID() {
+        return id;
     }
 
     @NotNull
