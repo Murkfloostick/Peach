bc4b937287e4a2b97f3b870cffe02a6dc32dc802
Labhatorian <16213031+Labhatorian@users.noreply.github.com>
2022-04-20 10:52:36 +0200

    Knop toegevoegd aan DPToevCompon

--- src/main/java/nl/windesheim/ictm2o/peach/DPToevCompon.java
+++ src/main/java/nl/windesheim/ictm2o/peach/DPToevCompon.java
@@ -2,10 +2,21 @@ package nl.windesheim.ictm2o.peach;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class DPToevCompon extends JPanel {
+public class DPToevCompon extends JPanel implements ActionListener {
+    private JButton toevoegen;
     public DPToevCompon(){
         setBackground(Color.gray);
         setPreferredSize(new Dimension(200, 550));
+        toevoegen = new JButton("Component toevoegen");
+        add(toevoegen);
+        toevoegen.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
