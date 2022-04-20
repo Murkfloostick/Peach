467560fdf21508fe9e1ad595f3c59b83cd1c5544
Frepds <94062636+frepds@users.noreply.github.com>
2022-04-20 12:08:07 +0200

    Commit new icons - Terry A Davis

--- src/main/java/nl/windesheim/ictm2o/peach/Main.java
+++ src/main/java/nl/windesheim/ictm2o/peach/Main.java
@@ -3,7 +3,6 @@ package nl.windesheim.ictm2o.peach;
 import javax.swing.*;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         System.setProperty("apple.laf.useScreenMenuBar", "true");
         System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Test");
@@ -12,5 +11,4 @@ public class Main {
         PeachWindow peachWindow = new PeachWindow();
         peachWindow.run();
     }
-
 }
