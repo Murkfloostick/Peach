8e37b9a330837a56b176d96d2500aeda74fe8fe9
Labhatorian <16213031+Labhatorian@users.noreply.github.com>
2022-04-21 12:12:57 +0200

    Beginnen met componenten toevoegen aan sleeppaneel

--- src/main/java/nl/windesheim/ictm2o/peach/DPComponPanel.java
+++ src/main/java/nl/windesheim/ictm2o/peach/DPComponPanel.java
@@ -6,14 +6,26 @@ import nl.windesheim.ictm2o.peach.components.RegisteredComponent;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class DPComponPanel extends JPanel {
     private int GLrows = 0;
     private ComponentRegistry CR;
+
+    //Dubbelklik om component toe te voegen aan sleeppaneel?
+    MouseListener ml = new MouseAdapter(){
+        public void mousePressed(MouseEvent me){
+            if(me.getClickCount() == 2)//double-click
+            {
+                System.out.println("Klik!");
+            }
+        }
+    };
 
     public DPComponPanel(ComponentRegistry CR){
         this.CR = CR;
@@ -21,7 +33,6 @@ public class DPComponPanel extends JPanel {
         setPreferredSize(new Dimension(200, 550));
         setMinimumSize(new Dimension(200, 550));
         setLayout(new GridLayout(GLrows, 2));
-
         refreshPanel();
     }
 
@@ -39,9 +50,12 @@ public class DPComponPanel extends JPanel {
         private BufferedImage image;
         public Image(RegisteredComponent RC) {
             try {
+                String iconnaam = RC.getIcon().name();
+                //Uncommenten zodra wij icons hebben
+                //image = ImageIO.read(new File("src/main/resources/Icons/" + iconnaam + ".png"));
                 image = ImageIO.read(new File("src/main/resources/Peach.png"));
             } catch (IOException ex) {
-                JOptionPane.showMessageDialog(null, ex.getMessage(), "InfoBox: " + ex.getCause(), JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(null, ex.getMessage(), "Hó daar: " + ex.getCause(), JOptionPane.INFORMATION_MESSAGE);
             }
         }
 
@@ -50,9 +64,8 @@ public class DPComponPanel extends JPanel {
             super.paintComponent(g);
 
             Graphics2D g2 = (Graphics2D) g;
-//            g2.setColor(getBackground());
-//            g2.fillRect(getWidth()/2, getHeight()/2, 20, 20);
-              g2.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), null);
+            g2.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), null);
+
         }
     }
 
@@ -60,11 +73,9 @@ public class DPComponPanel extends JPanel {
         //Vergroot plek
         GLrows += 1;
         setLayout(new GridLayout(GLrows, 2));
-
         add(new Image(RC));
         JLabel titleLabel = new JLabel(RC.getName(), SwingConstants.CENTER);
+        titleLabel.addMouseListener(ml);
         add(titleLabel);
-        //setPreferredSize(new Dimension(50, 50));
-
     }
 }
