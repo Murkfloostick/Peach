78885159e14c0f35a29fd62f33feae115f4452b5
frepds <94062636+frepds@users.noreply.github.com>
2022-04-21 13:00:46 +0200

    Commit Startpagina (minor changes)

--- src/main/java/nl/windesheim/ictm2o/peach/Main.java
+++ src/main/java/nl/windesheim/ictm2o/peach/Main.java
@@ -6,9 +6,14 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.setProperty("apple.laf.useScreenMenuBar", "true");
         System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Test");
-        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
         PeachWindow peachWindow = new PeachWindow();
+        //Fullscreen openen
+        peachWindow.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        //Deze alleen voor zonder borders: (nu false)
+        peachWindow.setUndecorated(false);
+        peachWindow.setVisible(true);
+
         peachWindow.run();
     }
 }
