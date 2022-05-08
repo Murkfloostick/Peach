a6b10f255fed1c6171680b92525a2a189c78c284
Tristan <code@thewoosh.org>
2022-05-08 18:23:55 +0200

    Improve main page

--- src/main/java/nl/windesheim/ictm2o/peach/StartPage.java
+++ src/main/java/nl/windesheim/ictm2o/peach/StartPage.java
@@ -1,43 +1,45 @@
 package nl.windesheim.ictm2o.peach;
 
+import net.miginfocom.swing.MigLayout;
 import nl.windesheim.ictm2o.peach.components.Design;
 import nl.windesheim.ictm2o.peach.storage.DesignFile;
 import nl.windesheim.ictm2o.peach.storage.ResourceManager;
+import org.jetbrains.annotations.NotNull;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 public class StartPage extends JPanel {
-    JPanel buttonContainer1 = new JPanel();
-    JPanel buttonContainer2 = new JPanel();
-    JPanel buttonContainer3 = new JPanel();
 
     private final PeachWindow m_parent;
-    private final Button m_monitorServicesButton;
-    private final Button m_newDesignButton;
-    private final Button m_openDesignButton;
-
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-
-    private static JLabel labelLinks;
-    private static JLabel labelCenter;
-    private static JLabel labelRechts;
+    private final Button monitorServicesButton;
+    private final Button newDesignButton;
+    private final Button openDesignButton;
 
     private static class Button extends JPanel {
 
-        public Button(String title) {
+        private final JButton jButton;
+
+        public Button(@NotNull String title, @NotNull BufferedImage image) {
+            setLayout(new MigLayout("al center center, wrap, gapy 20"));
+
+            jButton = new JButton(new ImageIcon(image));
+            add(jButton, "wrap");
+
             JLabel titleLabel = new JLabel(title, SwingConstants.CENTER);
             titleLabel.setFont(new Font("Arial", Font.BOLD, 25));
-            add(titleLabel);
-            setPreferredSize(new Dimension(550, 550));
+            add(titleLabel, "al center");
+        }
+
+        public void installMouseListener(MouseListener mouseListener) {
+            jButton.addMouseListener(mouseListener);
         }
 
     }
@@ -45,46 +47,27 @@ public class StartPage extends JPanel {
     public StartPage(PeachWindow parent) throws IOException {
         m_parent = parent;
 
-        m_monitorServicesButton = new Button("Monitor Services");
-        m_newDesignButton = new Button("Nieuw Ontwerp");
-        m_openDesignButton = new Button("Open Ontwerp");
+        setLayout(new MigLayout("insets 0 10% 0 10%"));
 
-        BufferedImage buttonIcon1 = ImageIO.read(ResourceManager.load("IconPack/Setting.png"));
-        button1 = new JButton();
-        button1.setPreferredSize(new Dimension(150, 150));
-        button1.setBorder(BorderFactory.createEmptyBorder());
-        button1.setContentAreaFilled(false);
-        button1 = new JButton(new ImageIcon(buttonIcon1));
-        add(button1);
+        JPanel logoPanel = new JPanel();
+        logoPanel.setBorder(new EmptyBorder(new Insets(30, 0, 30, 0)));
 
-        BufferedImage buttonIcon2 = ImageIO.read(ResourceManager.load("IconPack/Monitor.png"));
-        button2 = new JButton();
-        button2.setPreferredSize(new Dimension(150, 150));
-        button2.setBorder(BorderFactory.createEmptyBorder());
-        button2.setContentAreaFilled(false);
-        button2 = new JButton(new ImageIcon(buttonIcon2));
-        add(button2);
+        JLabel logoImageLabel = new JLabel(loadIcon());
+        logoPanel.add(logoImageLabel);
 
-        BufferedImage buttonIcon3 = ImageIO.read(ResourceManager.load("IconPack/OpenIcon.png"));
-        button3 = new JButton();
-        button3.setPreferredSize(new Dimension(150, 150));
-        button3.setBorder(BorderFactory.createEmptyBorder());
-        button3.setContentAreaFilled(false);
-        button3 = new JButton(new ImageIcon(buttonIcon3));
-        add(button3);
+        JLabel logoTextLabel = new JLabel("Peach");
+        logoTextLabel.setFont(new Font("Inter", Font.BOLD, 60));
+        logoPanel.add(logoTextLabel);
 
-        buttonContainer1.add(m_monitorServicesButton, button1);
-        buttonContainer2.add(m_newDesignButton, button2);
-        buttonContainer3.add(m_openDesignButton, button3);
+        add(logoPanel, "wrap");
 
-        //Layout voor windows
-        setLayout(new FlowLayout());
-        //Layout voor mac
-        setLayout(new GridLayout(2,3));
+        monitorServicesButton = new Button("Monitor Services", ImageIO.read(ResourceManager.load("IconPack/Monitor.png")));
+        newDesignButton = new Button("Nieuw Ontwerp", ImageIO.read(ResourceManager.load("IconPack/Setting.png")));
+        openDesignButton = new Button("Open Ontwerp", ImageIO.read(ResourceManager.load("IconPack/OpenIcon.png")));
 
-        add(buttonContainer1);
-        add(buttonContainer2);
-        add(buttonContainer3);
+        add(monitorServicesButton);
+        add(newDesignButton);
+        add(openDesignButton);
 
         installMouseListeners();
     }
@@ -92,8 +75,7 @@ public class StartPage extends JPanel {
     private void installMouseListeners() {
         StartPage startPage = this;
 
-        //m_monitorServicesButton
-        button1.addMouseListener(new MouseAdapter() {
+        monitorServicesButton.installMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 m_parent.openPage(startPage, "Dienstmonitor", new MonitorPage(m_parent));
@@ -101,8 +83,7 @@ public class StartPage extends JPanel {
             }
         });
 
-        //buttonContainer2
-        button2.addMouseListener(new MouseAdapter() {
+        newDesignButton.installMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 m_parent.openPage(startPage, "Ontwerper - Nieuw Ontwerp",
@@ -111,8 +92,7 @@ public class StartPage extends JPanel {
             }
         });
 
-        //m_openDesignButton
-        button3.addMouseListener(new MouseAdapter() {
+        openDesignButton.installMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 e.consume();
@@ -140,6 +120,13 @@ public class StartPage extends JPanel {
                 m_parent.openPage(startPage, "Ontwerper", new DesignPage(m_parent, m_parent, result.getDesign()));
             }
         });
+    }
+
+    @NotNull
+    private ImageIcon loadIcon() throws IOException {
+        final var loadedImage = new ImageIcon(ImageIO.read(ResourceManager.load("Peach.png")));
+        final var scaledImage = loadedImage.getImage().getScaledInstance(90, 90, Image.SCALE_DEFAULT);
+        return new ImageIcon(scaledImage);
     }
 
 }
