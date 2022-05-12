e8cfea3d12eee2bd91255d8ce9f8b57f68597b47
Labhatorian <16213031+Labhatorian@users.noreply.github.com>
2022-05-12 14:59:20 +0200

    Cleanup

--- src/main/java/nl/windesheim/ictm2o/peach/algorithm/BestAlgorithm.java
+++ src/main/java/nl/windesheim/ictm2o/peach/algorithm/BestAlgorithm.java
@@ -12,23 +12,11 @@ import java.util.List;
 import java.util.Map;
 
 public class BestAlgorithm {
-    private final Map<JLabel, PlacedComponent> map = new HashMap<>();
-    private final Map<PlacedComponent, ArrayList<PlacedComponent>> lineMap = new HashMap<>();
     private Design D;
 
     public BestAlgorithm(Design d) {
         D = d;
     }
-
-    public Map<JLabel, PlacedComponent> getMap() {
-        return map;
-    }
-
-    public Map<PlacedComponent, ArrayList<PlacedComponent>> getLineMap() {
-        return lineMap;
-
-    }
-
 
     float CA; //Current availbility
     float TA; //Target availbility
@@ -38,129 +26,6 @@ public class BestAlgorithm {
     List<PlacedComponent> masterARC; //Nieuwe components
     List<PlacedComponent> PC; //Components waar we het mee moeten doen
 
-    //TODO mogelijke oplossingen opslaan
-    //TODO Maximale aantalen per component instellen
-    //TODO mogelijke oplossingen comparen (welk is goedkoopst)
-
-    public void vindAv() {
-        int counter = 0; //Main counter om componenten toe te voegen
-        int counter2 = 0; //voor het toevoegen
-
-        //Voor de check of we alle componenten in lijst zijn aggegeaan
-        int totalSize = D.getPlacedComponents().size();
-        int usedCounter = 0;
-
-        //Initaliseer wat variabelen
-        CA = 0;
-        LA = 0;
-        PC = D.getPlacedComponents();
-        ARC = new ArrayList();
-        TA = D.getTargetAvailability();
-
-        //Terwijl availbility lager is dan target availbility
-        while (CA < D.getTargetAvailability()) {
-            //Zijn we aan einde van lijst? Voeg 1 component toe aan eerste en bij volgende de tweede...
-            if (totalSize < usedCounter) {
-                if (counter2 > totalSize) {
-                    counter2 = 0; //reset naar begin en voeg weer toe.
-                }
-
-                //Voeg component toe
-                Position pos = new Position(250, 250);
-                PlacedComponent NPC = new PlacedComponent(PC.get(counter2).getRegisteredComponent(), PC.get(counter2).getRegisteredComponent().getName(), pos);
-                ARC.add(NPC);
-
-                //Reset wat counters zodat we als het ware opnieuw beginnen
-                counter2 += 1;
-                usedCounter = 0;
-                counter = 0;
-            } else {
-                if (!(counter > totalSize - 1)) {
-                    //Voeg component toe
-                    Position pos = new Position(250, 250);
-                    PlacedComponent NPC = new PlacedComponent(PC.get(counter).getRegisteredComponent(), PC.get(counter).getRegisteredComponent().getName(), pos);
-                    ARC.add(NPC);
-                    usedCounter += 1;
-
-                    //Check of availbility nu lager is dan vorige
-                    CA = D.getAvailbility(ARC) * 100;
-                    if (CA < LA) {
-                        //Als dat zo is, gaan we 1 stap terug en gaan we naar de volgende component.
-                        ARC.remove(NPC);
-                        counter += 1;
-                    } else if (CA >= TA) {
-                        //Anders gaan we kijken of we het kunnen toevoegen als oplossing
-                        checkAndAdd();
-                        LA = CA;
-                    } else {
-                        //Sla laatste availbility op
-                        LA = CA;
-                    }
-                } else {
-                    counter = 0;
-                }
-            }
-        }
-            checkAndAdd();
-        }
-
-    public void checkAndAdd(){
-        //Als ze hetzelfde zijn, stop en geef de nieuwe components door aan Design
-        int allUsed = 0;
-        for (PlacedComponent PC:D.getPlacedComponents()
-        ) {
-            //via een teller
-            for (PlacedComponent arcPC:ARC
-            ) {
-                if(PC.getRegisteredComponent() == arcPC.getRegisteredComponent()){
-                    allUsed +=1;
-                }
-            }
-        }
-        if(D.getPlacedComponents().size() == allUsed){
-            D.deletePlacComponentList();
-            D.newPlacComponentList(ARC);
-        } else{
-            vindAv();
-        }
-    }
-
-    public void vindAv2(){
-        CA = 0;
-        LA = 0;
-        PC = D.getPlacedComponents();
-        ARC = new ArrayList();
-        TA = D.getTargetAvailability();
-
-        int counter = 0;
-        int counter2 = 0;
-        while(TA > CA){
-            if(counter == D.getPlacedComponents().size()){
-                //Voeg component toe
-                Position pos2 = new Position(250, 250);
-                PlacedComponent NPC2 = new PlacedComponent(PC.get(counter2).getRegisteredComponent(), PC.get(counter2).getRegisteredComponent().getName(), pos2);
-                ARC.add(NPC2);
-
-                counter2 += 1;
-                if(counter2 == D.getPlacedComponents().size()){
-                    counter2 = 0;
-                }
-                counter = 0;
-            } else {
-                Position pos = new Position(250, 250);
-                PlacedComponent NPC = new PlacedComponent(PC.get(counter).getRegisteredComponent(), PC.get(counter).getRegisteredComponent().getName(), pos);
-                ARC.add(NPC);
-                counter += 1;
-
-                CA = D.getAvailbility(ARC) * 100;
-                if (CA < LA) {
-                    ARC.remove(NPC);
-                } else {
-                    LA = CA;
-                }
-            }
-        }
-    }
 
     public void vindAv3(){
         CA = 0;
