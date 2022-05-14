36e96ec82d12a705adb00ea075483ffe8c0e6e25
Labhatorian <16213031+Labhatorian@users.noreply.github.com>
2022-05-14 13:58:08 +0200

    Algoritme klaar?

--- src/main/java/nl/windesheim/ictm2o/peach/algorithm/BestAlgorithm.java
+++ src/main/java/nl/windesheim/ictm2o/peach/algorithm/BestAlgorithm.java
@@ -136,19 +136,20 @@ public class BestAlgorithm {
     }
 
     public void aaaaa(){
-        //TODO Verhogen van maximaal aantal componenten
         //Variabelen initalisatie
         CA = 0;
+        TA = D.getTargetAvailability();
         PC = D.getPlacedComponents();
+
         ARC = new ArrayList();
         List<PlacedComponent> tempArc = new ArrayList();
         ArrayList<List<PlacedComponent>> masterARC = new ArrayList<>(); //Lijst met alle oplossingen
-        TA = D.getTargetAvailability();
+
         PlacedComponent main;
         PlacedComponent main2;//De tweede die wordt gebruikt
-        int counter2 = 0;
 
         int max = 3; //Max aantal componenten van dezelfde RegisterdeComponent
+        int maxtemp = 1; // begin hier
 
         //Plaats eerst alle componenten
         for (PlacedComponent PC:PC
@@ -159,63 +160,75 @@ public class BestAlgorithm {
 
         masterARC.add(ARC);//Dit is een oplossing
 
+        //Voor elk element in de componenten die zijn geplaatst
         for (int counter = 0; counter <= PC.size()-1; counter++) {
             //De eerste waar we het mee gaan doen
             main = PC.get(counter);
 
-            //Eerst plaatsen en dan ze allemaal en dan een voor een
-            ARC.add(new PlacedComponent(main.getRegisteredComponent(), main.getName(), main.getPosition()));
+            //*** MOGELIJK NIET NODIG?
+            //Eerst max keer plaatsen en dan ze allemaal en dan een voor een
+            for (int maxcount = maxtemp; maxcount <= max; maxcount++) {
+                ARC.add(new PlacedComponent(main.getRegisteredComponent(), main.getName(), main.getPosition()));
+            }
             masterARC.add(ARC); //Dit is een oplossing
 
-            //En dan de rest
+            //En dan de rest 1 keer plaatsen
             for (int count = 0; count <= PC.size() - 1; count++) {
                 if(count != counter){ //Voorkom dat we Main weer plaatsen
                     ARC.add(new PlacedComponent(PC.get(count).getRegisteredComponent(), PC.get(count).getName(), PC.get(count).getPosition()));
                 }
             }
             masterARC.add(ARC); //Dit is een oplossing
+            //***
 
-            //Haal het leeg
+            //Haal het leeg en terug naar begin
             ARC = null;
             ARC = new ArrayList<>(tempArc);
 
-            //Plaats de eerste
-            ARC.add(new PlacedComponent(main.getRegisteredComponent(), main.getName(), main.getPosition()));
+            //Plaats de eerste, Begin bij max 2 en dan 3...
+            for (int maxcount = maxtemp; maxcount <= max; maxcount++) {
+                ARC.add(new PlacedComponent(main.getRegisteredComponent(), main.getName(), main.getPosition()));
+            }
 
-            //Dan de tweede, terug, derde enzovoort
+            //Dan de tweede, terug, derde enzovoort zodat we iedereen langs gaan
             for (int count = 0; count <= PC.size() - 1; count++) {
                 if(count != counter) { //Voorkom dat we Main weer plaatsen
                     main2 = PC.get(count);//Hou de tweede vast en doe de volgende componenten eerst
 
                     //Plaats max keer, dan minder tot er maar 1 is en dan volgende component die main2 wordt
                     //MAX begint bij 2 tot de echte max
-                    if(main != main2){
-                        //plaats max keer
+                    if(main != main2){//Main is al maximaal geplaatst
+                        for (int maxcount = maxtemp; maxcount <= max; maxcount++) {
+                            PlacedComponent plaats = new PlacedComponent(PC.get(count).getRegisteredComponent(), PC.get(count).getName(), PC.get(count).getPosition());
+                            ARC.add(plaats);
+                            masterARC.add(ARC);
+                            ARC.remove(plaats);
+                        }
                     }
-                    //dan for elk ander component dat niet main en main2 is plaatsen
-                    PlacedComponent plaats = new PlacedComponent(PC.get(count).getRegisteredComponent(), PC.get(count).getName(), PC.get(count).getPosition());
-                    ARC.add(plaats);
-                    masterARC.add(ARC);
-                    ARC.remove(plaats);
 
-                    //Volgende is main en hetzelfde
+                    //Nu de volgende component steeds tot max en dan verwijderen
+                    for (PlacedComponent PC:PC
+                         ) {
+                        if(PC != main && PC != main2){
+                            for (int maxcount = maxtemp; maxcount <= max; maxcount++) {
+                                //dan for elk ander component dat niet main en main2 is plaatsen
+                                PlacedComponent plaats = new PlacedComponent(PC.get(count).getRegisteredComponent(), PC.get(count).getName(), PC.get(count).getPosition());
+                                ARC.add(plaats);
+                                masterARC.add(ARC);
+                            }
+                            //Verwijder nu maxtemp keer
+                            for (int maxremover = 1; maxremover <= maxtemp; maxremover++){
+                                ARC.remove(ARC.size() - maxremover);
+                            }
+                        }
+                    }
+                    maxtemp += 1; //Nu worden de volgende componenten met 1 max verhoogd zodat we elk combinatie vinden
                 }
             }
 
             //Haal het leeg
             ARC = null;
             ARC = new ArrayList<>(tempArc);
-
-            //Nog een keer doen maar dan met een extra component totdat max is gehaald
-            if(counter+1 <= PC.size()-1){
-
-            }
-        }
-
-        //Print nu alles in masterarc
-        for (List<PlacedComponent> ARC : masterARC
-        ) {
-            System.out.println(ARC.toString());
         }
     }
         }
